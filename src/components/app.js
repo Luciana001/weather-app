b6d03db36c6cc873e0e84b196ecbe3895d2bd3a3
Luciana001 <luciana.brancato@gmail.com>
2021-05-03 14:29:43 +0200

    installation package

---
+++ src/components/app.js
@@ -0,0 +1,16 @@
+import { react } from "babel-types";
+import React from "react";
+import { render } from "react-dom";
+
+const App = () => {
+    // render () {
+
+    // }
+    return (
+        <div>
+            <h1>Coucou</h1>
+        </div>
+    )
+}
+
+export default App
