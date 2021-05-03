c24464ed1de535dce40cbe8bc4e3c47ceb82693a
Luciana001 <luciana.brancato@gmail.com>
2021-05-03 17:08:13 +0200

    structure + html

--- src/components/app.js
+++ src/components/app.js
@@ -1,16 +1,49 @@
 import { react } from "babel-types";
+import { doc } from "prettier";
 import React from "react";
 import { render } from "react-dom";
 
-const App = () => {
-    // render () {
 
-    // }
-    return (
-        <div>
-            <h1>Coucou</h1>
-        </div>
-    )
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    state = {
+        ville: "",
+       
+    }
+
+    handleVille = () => {
+        let { ville } = this.state;
+        
+        
+        ville = document.querySelector("#ville").value;
+
+        console.log(ville)
+        
+
+
+
+        fetch(url)
+            .then(response => response.json())
+            .then(data => console.log(data));
+    }
+    
+
+
+    render() {
+        return (
+            <div>
+                <h2>Météo par ville</h2>
+                <input type="text" id="ville" placeholder="Entrez une ville"></input>
+                <button type="submit" id="send" onClick={this.handleVille}>Send</button>
+                <p></p>
+            </div >
+
+        )
+
+    }
+
 }
-
 export default App
